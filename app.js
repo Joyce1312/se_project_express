6de613191817bc3cbf470c67d82a6a03df6a4f5f
Joyce1312 <joyce1312183j@gmail.com>
2025-11-18 00:51:25 -0500

    set up project structure and connect MongoDB with Mongoose

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to db");
+  })
+  .catch(console.error);
 
 const { PORT = 3001 } = process.env;
 
