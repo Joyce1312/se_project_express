0c833ed73f66affd4550d57f380a6870a91eece6
Joyce1312 <joyce1312183j@gmail.com>
2025-11-19 00:17:20 -0500

    feat: add likes routes

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -11,6 +11,10 @@ router.post("/", createClothingItem);
 
 router.delete("/:itemId", deleteClothingItem);
 
+router.put("/:itemId/likes");
+
+router.delete("/:itemId/likes");
+
 router.use((req, res) => {
   res.status(404).send({ message: "Requested resource not found" });
 });
