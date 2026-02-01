2fef103e0911e80df351e7f7471ad5378e8b03d7
Joyce1312 <joyce1312183j@gmail.com>
2026-02-01 14:07:41 -0500

    feat: adding evironmrntal variables

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,6 @@
-const JWT_SECRET = "secret-key";
+// Looks for JWT_SECRET in the environment variables
+// Sets a fallback value if JWT_SECRET doesn't exist
+const { JWT_SECRET = "super-strong-secret" } = process.env;
 
 module.exports = {
   JWT_SECRET,
